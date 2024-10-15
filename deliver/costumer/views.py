42bd269000b02c8626c5ab067f9ff3469b4fe729
Pablo Cacciagioni <pablo1234caccia@gmail.com>
2024-10-15 16:41:45 -0300

    Add the templates

--- deliver/costumer/views.py
+++ deliver/costumer/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'costumer/index.html')
+    
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'costumer/about.html')
